23d9ba0f6fa94e0bb7f7223c6d0f4518612b3e8d
Aluis Marte <aluismarte@gmail.com>
2022-01-22 20:07:46 -0400

    Adding debug console text on end run.

--- FPSCounter.cs
+++ FPSCounter.cs
@@ -1,11 +1,29 @@
-using Godot;
 using System;
+using Godot;
 
 public class FPSCounter : Label
 {
+    private float _maxFps = 0;
+    private float _fps = 0;
 
-	public override void _Process(float delta)
-	{
-		Text = "FPS: " + Performance.GetMonitor(Performance.Monitor.TimeFps);
-	}
+    private int count = 0;
+
+    public override void _Process(float delta)
+    {
+        var timeFps = Performance.GetMonitor(Performance.Monitor.TimeFps);
+        if (_maxFps < timeFps)
+        {
+            _maxFps = timeFps;
+        }
+
+        count++;
+        _fps += timeFps;
+        Text = "FPS: " + timeFps;
+    }
+
+    public override void _ExitTree()
+    {
+        Console.Out.WriteLine("Max FPS: " + _maxFps);
+        Console.Out.WriteLine("FPS Average: " + (_fps / count));
+    }
 }
---
+++ README.md
@@ -0,0 +1,19 @@
+﻿# Performance Visual script vs GDScript on Godot
+
+Some elements are write on C# to skip this ok performance test (FPS Counter and flashlight)
+
+
+Benchmark
+
+# Tested on:
+ - Intel Core I7 11370H 3.3 Ghz
+ - 16 GB Ram DDR4 3200
+ - NVidia Geforce RTX 3060 Laptop GPU
+
+# Results
+
+| Language      | Max FPS | Stable FPS |
+|---------------|:-------:|:----------:|
+| Visual Script |   856   |
+| GDScript      |   833   |
+
